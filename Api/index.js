b83463ddc02f14780977cad6d241ab6b69eb4ac1
KenetGaona115 <aldrinkenet@hotmail.com>
2023-11-01 22:31:54 -0600

    ng build success/ backend error

--- Api/index.js
+++ Api/index.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser')
 const app = express();
 //setings
 app.listen(process.env.PORT || 3000, () => console.log('Listening'));
-// app.use(express.static(path.join(__dirname, 'public/frontend/')))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,10 +16,10 @@ app.use('/api/v1/furniture', require('./routes/furniture'))
 //middleware
 app.use(express.urlencoded({ extended: false }));
 
-//static
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/public/index.html'))
-// })
+// static
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/aa/index.html'))
+})
 
 module.exports = app;
